397a73232a89513d6d738a8586f16f6d4ca7b92f
Esmeralda <almeiidaesmeralda23@gmail.com>
2021-06-26 16:00:28 -0300

    ProjetoAPIGit

---
+++ ProjetoAPIGit/ProjetoAPIGit/Context/Context.cs
@@ -0,0 +1,14 @@
+﻿using ProjetoAPIGit.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoAPIGit.Context
+{
+    public class Context : DbContext
+    { 
+        public DbSet<Favorito> tbFavorito { get; set; }
+    }
+}
---
+++ ProjetoAPIGit/ProjetoAPIGit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjetoAPIGit.Startup))]
+namespace ProjetoAPIGit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
